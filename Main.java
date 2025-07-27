83743f21f4f0a61845696a2d549450bebd931212
Lucas <knightfication@gmail.com>
2025-07-27 16:57:12 -0500

    Randomize class now returns one villain from the list of villains, or hero, or an expansion.

--- Main.java
+++ Main.java
@@ -2,9 +2,12 @@ public class Main {
     public static void main(String[] args){
         ExpansionsList expansionList = new ExpansionsList();
         CharactersList charactersList = new CharactersList();
+        Randomize rand = new Randomize();
         System.out.println(expansionList.getExpansions());
         System.out.println(charactersList.getHeroes());
         System.out.println(charactersList.getVillains());
-
+        System.out.println(rand.getExpansion());
+        System.out.println(rand.getHero());
+        System.out.println(rand.getVillain());
     }
 }
--- Randomize.java
+++ Randomize.java
@@ -1,11 +1,31 @@
+import java.util.Collections;
+import java.util.List;
+
 public class Randomize{
 
     ExpansionsList expansionsList = new ExpansionsList();
     CharactersList charactersList = new CharactersList();
+    List<String> expansions = expansionsList.getExpansions();
+    List<String> heroes = charactersList.getHeroes();
+    List<String> villains = charactersList.getVillains();
 
-    
-    public void randomize(){
+    public void randomize(List<String> list){
+        Collections.shuffle(list);
+    }
 
+    public String getExpansion(){
+        randomize(expansions);
+        return expansions.get(0);
+    }
+
+    public String getHero(){
+        randomize(heroes);
+        return heroes.get(0);
+    }
+
+    public String getVillain(){
+        randomize(villains);
+        return villains.get(0);
     }
 
 }
