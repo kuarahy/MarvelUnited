76dd32a506855747686f3bf778652e6463bee0ee
Lucas <knightfication@gmail.com>
2025-07-27 13:46:20 -0500

    Added three classes. Just adding strings to ExpasionsList and Characters for now.

---
+++ Characters.java
@@ -0,0 +1,16 @@
+public class Charactes extends ExpansionsList {
+    public final String spiderMan = "Spider-Man";
+    public final String milesMorales = "Miles Morales";
+    public final String ghostSpider = "Ghost Spider";
+    public final String ironMan = "Iron-Man";
+    public final String captainAmerica = "Captain America";
+    public final String blackWidow = "Black Widow";
+    public final String hulk = "Hulk";
+    public final String captainMarvel = "Captain Marvel";
+    public final String antMan = "Ant-Man";
+    public final String wasp = "Wasp";
+    public final String vRedSkull = "RedSkull";
+    public final String vUltron = "Ultron";
+    public final String vTaskmaster = "Taskmaster";
+    public final String 
+}
---
+++ ExpansionsList.java
@@ -0,0 +1,15 @@
+/*
+ * A class that holds every single expansion I have. 
+ * Extend this class with your characters so you can randomize it to your needs
+ */
+
+public class ExpansionsList {
+
+    /* Enter the Spider-Verse */
+    public final String enterTheSpiderVerse = "Enter the Spider-Verse";
+    public final String coreBoxAvengers = "Avengers Core Box";
+    public final String xMenTheHorsemenOfApocalypse = "X-MEN The Horsemen of Apocalypse";
+    public 
+    
+
+}
